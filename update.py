b36974e1b058e1802dc791c340956fac64312621
Jordan Petersen <crab8011@gmail.com>
2018-09-26 13:32:03 -0600

    Updated the updator and added in a test.

--- update.py
+++ update.py
@@ -1,15 +1,39 @@
 import requests
 
-packInfo = 'https://raw.githubusercontent.com/The-Blue-Sakura/ARView/master/README.md'
+packInfo = 'https://raw.githubusercontent.com/The-Blue-Sakura/ARView/master/upd.8'
 
 r = requests.get(packInfo)
-print r.text
+print 'Downloading file upd.8'
 
-f = open("readme.md", "w")
+f = open("upd.8", "w")
 f.write(r.text)
 f.close()
-print 'File Written and Closed. New File:'
+print "File Saved: upd.8"
 
-f = open("readme.md", "r")
-print f.read()
-f.close()
+f = open("upd.8", "r")
+cverfile = open("currentversion", "r")
+
+updateVersion = f.readline()
+currentVersion = cverfile.readline()
+cverfile.close()
+print "UPDATE VERSION: "
+print updateVersion
+print "CURRENT VERSION: "
+print currentVersion
+
+if(int(updateVersion) > int(currentVersion)):
+    print "UPDATE REQUIRED. UPDATING!"
+    while True:
+        filename = f.readline()
+        if(filename == 'EOF'):
+            break
+        fileurl = f.readline()
+        dlBuffer = requests.get(fileurl)
+        dlFile = open("filename", w)
+        dlFile.write(dlBuffer.text)
+        dlFile.close()
+    cverfile = open("currentversion", "w")
+    cverfile.write(updateVersion)
+    cverfile.close()
+else:
+    print "UP TO DATE"
