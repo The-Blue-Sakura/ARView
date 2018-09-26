320a731212455d64c4d834b96168a16c1777b5ac
Jordan Petersen <crab8011@gmail.com>
2018-09-26 13:45:35 -0600

    Testing the updater's ability to update itself.

--- update.py
+++ update.py
@@ -9,7 +9,7 @@ f = open("upd.8", "w")
 f.write(r.text)
 f.close()
 print "File Saved: upd.8"
-
+print "CAN THE UPDATER UPDATE THE UPDATER"
 f = open("upd.8", "r")
 cverfile = open("currentversion", "r")
 
@@ -23,15 +23,19 @@ print currentVersion
 
 if(int(updateVersion) > int(currentVersion)):
     print "UPDATE REQUIRED. UPDATING!"
+    filename = f.readline().strip("\n")
     while True:
-        filename = f.readline()
-        if(filename == 'EOF'):
-            break
-        fileurl = f.readline()
+        print filename
+        fileurl = f.readline().strip("\n")
+        print fileurl
         dlBuffer = requests.get(fileurl)
-        dlFile = open("filename", w)
+        dlFile = open(filename, "w")
         dlFile.write(dlBuffer.text)
         dlFile.close()
+        filename = f.readline().strip("\n")
+        print filename == 'EOF'
+        if(filename == 'EOF'):
+            break
     cverfile = open("currentversion", "w")
     cverfile.write(updateVersion)
     cverfile.close()
