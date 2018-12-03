90635311f984985486250ebfbf8154f62188423e
Jordan Petersen <crab8011@gmail.com>
2018-12-03 15:28:05 -0700

    Created a new utilities class and updated the time applet.

--- HeadsetProgram/src/timeApplet.py
+++ HeadsetProgram/src/timeApplet.py
@@ -1,35 +1,27 @@
 from multiprocessing import Process
+from SystemUtilities import Format
 import time
 import datetime
 
 class TimeApplet():
+    APPDISPLAYMODE = 0
+    NOTIFICATIONDISPLAYMODE = 0
+
     amPM = "N/A"
-    now = "%s:%s:%s %s" % (TimeApplet.formatHour(), datetime.datetime.now().minute, datetime.datetime.now().second, amPM)
+    now = "%s:%s:%s %s" % (Format.timeTwelveHour()[0], datetime.datetime.now().minute, datetime.datetime.now().second, Format.timeTwelveHour()[1])
 
     def appletLoad(self):
         self.running = True
-        self.current_time = self.now
+        self.current_time = TimeApplet.now
         self.lastTime = "NULL"
 
     def appletMain(self):
         updateTimeProcess = Process(target=self.updateTime, args=())
         updateTimeProcess.start()
 
-    @classmethod
-    def formatHour(cls):
-        hour = datetime.datetime.now().hour
-        newHour = 0
-        if hour > 12:
-            newHour = hour - 12
-            cls.amPM = "PM"
-        else:
-            newHour = hour
-            cls.amPM = "AM"
-        return newHour
-        
     def updateTime(self):
         while self.running:
-            self.now = "%s:%s:%s %s" % (TimeApplet.formatHour(), datetime.datetime.now().minute, datetime.datetime.now().second, self.amPM)
+            self.now = "%s:%s:%s %s" % (Format.timeTwelveHour()[0], datetime.datetime.now().minute, datetime.datetime.now().second, Format.timeTwelveHour()[1])
             if self.current_time != self.lastTime:
                 self.lastTime = self.current_time
             self.current_time = self.now
@@ -43,6 +35,9 @@ class TimeApplet():
     
     @classmethod
     def getAppletDisplay(cls):
+        return cls.now
+    @classmethod
+    def getAppletNotification(cls):
         return cls.now
 
     def __init__(self):
