213d90be76bb0c84d40fa89e12abd9e6f7c6df14
Jordan Petersen <crab8011@gmail.com>
2019-02-12 14:14:56 -0700

    Cleaned out old unused code and moved src out of HeadsetProgram2

--- HeadsetProgram/src/ENVSetup.py
+++
@@ -1,9 +0,0 @@
-'''
-We need to install python-3, python3-pip.
-We then need to install the OpenCV, Numpy, picamera, and "picamera[array]" packages through PIP.
-'''
-
-# Import OpenCV
-import cv2
-# Print the version of OpenCV that we are using, to help us find the right documentation.
-print(cv2.__version__)
