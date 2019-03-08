3dd3d2890b50a7211c7889fb3a2db69e0f66809a
Jordan Petersen <crab8011@gmail.com>
2019-03-07 23:56:29 -0700

    Started working with the bluetooth rfcomm0 file

---
+++ srcC/main2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+    FILE *ptr_file;
+    char buf[1000];
+
+    ptr_file = fopen("/dev/rfcomm0", "r");
+
+    if(!ptr_file){
+        return 1;
+    }
+
+    while(fgets(buf, 1000, ptr_file)!=NULL){
+        printf("%s", buf);
+    }
+    fclose(ptr_file);
+    return 0;
+}
